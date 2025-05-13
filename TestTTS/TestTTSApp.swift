0bc51edef337f9eb0e3b0c0fe0e38fdf7b6eba3a
Kong <kong@github.zz>
2025-05-13 17:02:32 +0800

    Initial Commit

---
+++ TestTTS/TestTTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTTSApp.swift
+//  TestTTS
+//
+//  Created by apple on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TestTTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestTTSTests/TestTTSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTTSTests.swift
+//  TestTTSTests
+//
+//  Created by apple on 5/13/25.
+//
+
+import Testing
+@testable import TestTTS
+
+struct TestTTSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
